7aaa64e12b3a49a2430eb960a7f58162d6c8dab3
Caique Almeida <caaguima@gmail.com>
2020-09-29 22:54:44 -0300

    Initiated ExpensesList

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react'
+import ExpensesList from './components/ExpensesList'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Uma Só</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Uma Só</h1>
+            <ExpensesList />
+        </div>
+    )
 }
 
-export default App;
+export default App
